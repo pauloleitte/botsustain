55a455809deb467e77cf6ef4aed1eb56d443e510
P. Gonçalves Leite <p.goncalves.leite@avanade.com>
2019-02-14 19:14:29 -0200

    1.4

--- src/bot.js
+++ src/bot.js
@@ -11,21 +11,26 @@ const bot = new Telegraf(env.token)
 const telegram = new Telegram(env.token)
 
 //Declarando as váriaveis.
-var listaChamadosdeAuto = []
-var listaChamadosdeMassificados = []
+var listaChamadosAuto = []
+var listaChamadosCargas = []
+var listaChamadosWebMethods = []
+var listaChamadosMassificados = []
 var listaChamadosFechadosDiario = []
 var listaChamadosFechadosMensal = []
 
+
 //Criando a regra de schedule.
 var rule = new schedule.RecurrenceRule();
-rule.dayOfWeek = [ 1, 2, 3, 4, 5];
+rule.dayOfWeek = [1, 2, 3, 4, 5];
 rule.hour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-rule.minute = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+rule.minute = [5, 15, 25, 35, 45, 55];
 
 //Iniciando o Job schedule.
 var j = schedule.scheduleJob(rule, async () => {
     await enviarChamadosAuto();
     await enviarChamadosMassificados();
+    await enviarChamadosCargas();
+    await enviarChamadosWebMethods();
 });
 
 //Método que realiza o envio dos chamados para o grupo de auto via telegram.
@@ -33,9 +38,9 @@ const enviarChamadosAuto = async () => {
     await getChamadosAuto()
     const now = moment().format("DD/MM/YYYY")
     var contador = 0;
-    listaChamadosdeAuto.map(chamado => {
+    listaChamadosAuto.map(chamado => {
         if (chamado.DataNextBreachOLA != null) {
-            if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                 contador = contador + 1;
             }
         }
@@ -50,9 +55,9 @@ const enviarChamadosMassificados = async () => {
     await getChamadosMassificados()
     const now = moment().format("DD/MM/YYYY")
     var contador = 0;
-    listaChamadosdeMassificados.map(chamado => {
+    listaChamadosMassificados.map(chamado => {
         if (chamado.DataNextBreachOLA != null) {
-            if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                 contador = contador + 1;
             }
         }
@@ -61,6 +66,40 @@ const enviarChamadosMassificados = async () => {
         await telegram.sendMessage(env.groupMassificadoId, `Temos ${contador} para vencer na data de ${now}`)
     }
 
+}
+//Método que realiza o envio dos chamados para o grupo de cargas via telegram.
+const enviarChamadosCargas = async () => {
+    await getChamadosCargas()
+    const now = moment().format("DD/MM/YYYY")
+    var contador = 0;
+    listaChamadosCargas.map(chamado => {
+        if (chamado.DataNextBreachOLA != null) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                contador = contador + 1;
+            }
+        }
+    })
+    if (contador != 0) {
+        await telegram.sendMessage(env.groupAutoId, `Temos ${contador} para vencer na data de ${now}`)
+    }
+
+}
+
+//Método que realiza o envio dos chamados para o grupo de webmethods via telegram.
+const enviarChamadosWebMethods = async () => {
+    await getChamadosWebMethods()
+    const now = moment().format("DD/MM/YYYY")
+    var contador = 0;
+    listaChamadosWebMethods.map(chamado => {
+        if (chamado.DataNextBreachOLA != null) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                contador = contador + 1;
+            }
+        }
+    })
+    if (contador != 0) {
+        await telegram.sendMessage(env.groupAutoId, `Temos ${contador} para vencer na data de ${now}`)
+    }
 }
 
 //Realiza a chamada na API para recolher os dados de Chamados Fechados Diario.
@@ -79,15 +118,27 @@ const getChamadosFechadosMensal = async () => {
 
 //Realiza a chamada na API para recolher a lista de chamados de Auto.
 const getChamadosAuto = async () => {
-    await axios.get(env.apiChamados + "6").then(resp => {
-        listaChamadosdeAuto = resp.data.Table
+    await axios.get(env.apiChamados + "2").then(resp => {
+        listaChamadosAuto = resp.data
     }).catch(e => console.log(e))
 }
 
 //Realiza a chamada na API para recolher a lista de chamado de Massificados.
 const getChamadosMassificados = async () => {
-    await axios.get(env.apiChamados + "7").then(resp => {
-        listaChamadosdeMassificados = resp.data.Table
+    await axios.get(env.apiChamados + "3").then(resp => {
+        listaChamadosMassificados = resp.data
+    }).catch(e => console.log(e))
+}
+
+const getChamadosCargas = async () => {
+    await axios.get(env.apiChamados + "4").then(resp => {
+        listaChamadosCargas = resp.data
+    }).catch(e => console.log(e))
+}
+
+const getChamadosWebMethods = async () => {
+    await axios.get(env.apiChamados + "5").then(resp => {
+        listaChamadosWebMethods = resp.data
     }).catch(e => console.log(e))
 }
 
@@ -95,8 +146,10 @@ const getChamadosMassificados = async () => {
 const main = async () => {
     await getChamadosAuto();
     await getChamadosMassificados();
-    await getChamadosFechadosMensal();
-    await getChamadosFechadosDiario();
+    await getChamadosCargas();
+    await getChamadosWebMethods();
+    //await getChamadosFechadosMensal();
+    //await getChamadosFechadosDiario();
 }
 
 //Comando de start do bot.
@@ -110,21 +163,22 @@ bot.command("dauto", async ctx => {
     await getChamadosAuto()
     const now = moment().format("DD/MM/YYYY")
     var existeChamado = false;
-    listaChamadosdeAuto.map(chamado => {
+    listaChamadosAuto.map(chamado => {
         if (chamado.DataNextBreachOLA != null) {
-            if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                 existeChamado = true;
             }
         }
     })
     if (existeChamado) {
-        await listaChamadosdeAuto.map(chamado => {
+        await listaChamadosAuto.map(chamado => {
             if (chamado.DataNextBreachOLA != null) {
-                if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+                if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                    var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
                     ctx.reply(`Incidente: ${chamado.IdIncidente} 
 Titulo: ${chamado.Titulo} 
 Prioridade: ${chamado.Prioridade} 
-Vencimento: ${chamado.DataNextBreachOLA}
+Vencimento: ${vencimento}
 Status: ${chamado.Status}`)
                 }
             }
@@ -140,21 +194,22 @@ bot.command("dmassificado", async ctx => {
     await getChamadosMassificados()
     const now = moment().format("DD/MM/YYYY")
     var existeChamado = false;
-    listaChamadosdeMassificados.map(chamado => {
+    listaChamadosMassificados.map(chamado => {
         if (chamado.DataNextBreachOLA != null) {
-            if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                 existeChamado = true
             }
         }
     })
     if (existeChamado) {
-        await listaChamadosdeMassificados.map(chamado => {
+        await listaChamadosMassificados.map(chamado => {
             if (chamado.DataNextBreachOLA != null) {
-                if (chamado.DataNextBreachOLA.substring(0, 10) == now) {
+                if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                    var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
                     ctx.reply(`Incidente: ${chamado.IdIncidente}
 Titulo: ${chamado.Titulo}
 Prioridade: ${chamado.Prioridade}
-Vencimento: ${chamado.DataNextBreachOLA}
+Vencimento: ${vencimento}
 Status: ${chamado.Status}`)
                 }
             }
@@ -162,6 +217,68 @@ Status: ${chamado.Status}`)
     }
     else {
         ctx.reply(`Não há chamados de Massificados para ${now}.`)
+    }
+})
+
+//Retorna a lista de chamados de Cargas através do comando.
+bot.command("dcargas", async ctx => {
+    await getChamadosCargas()
+    const now = moment().format("DD/MM/YYYY")
+    var existeChamado = false;
+    listaChamadosCargas.map(chamado => {
+        if (chamado.DataNextBreachOLA != null) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                existeChamado = true
+            }
+        }
+    })
+    if (existeChamado) {
+        await listaChamadosCargas.map(chamado => {
+            if (chamado.DataNextBreachOLA != null) {
+                if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                    var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
+                    ctx.reply(`Incidente: ${chamado.IdIncidente}
+Titulo: ${chamado.Titulo}
+Prioridade: ${chamado.Prioridade}
+Vencimento: ${vencimento}
+Status: ${chamado.Status}`)
+                }
+            }
+        })
+    }
+    else {
+        ctx.reply(`Não há chamados de Cargas para ${now}.`)
+    }
+})
+
+//Retorna a lista de chamados de WebMethods através do comando.
+bot.command("dwebmethods", async ctx => {
+    await getChamadosWebMethods()
+    const now = moment().format("DD/MM/YYYY")
+    var existeChamado = false;
+    listaChamadosWebMethods.map(chamado => {
+        if (chamado.DataNextBreachOLA != null) {
+            if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                existeChamado = true
+            }
+        }
+    })
+    if (existeChamado) {
+        await listaChamadosWebMethods.map(chamado => {
+            if (chamado.DataNextBreachOLA != null) {
+                if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
+                    var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
+                    ctx.reply(`Incidente: ${chamado.IdIncidente}
+Titulo: ${chamado.Titulo}
+Prioridade: ${chamado.Prioridade}
+Vencimento: ${vencimento}
+Status: ${chamado.Status}`)
+                }
+            }
+        })
+    }
+    else {
+        ctx.reply(`Não há chamados de Webmethods para ${now}.`)
     }
 })
 
