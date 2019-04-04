eb4c92c23232297fa1a408ae7e7b4872465e7c38
pauloleitte <paulo.leitte@live.com>
2019-04-04 18:50:20 -0300

    Melhorias nos comandos

--- src/bot.js
+++ src/bot.js
@@ -161,6 +161,7 @@ const main = async () => {
     await getChamadosMassificados();
     await getChamadosCargas();
     await getChamadosWebMethods();
+    await getChamadosGeral();
     //await getChamadosFechadosMensal();
     //await getChamadosFechadosDiario();
     console.log("Bot iniciado com sucesso")
@@ -170,6 +171,19 @@ const main = async () => {
 bot.start(async ctx => {
     const nome = ctx.update.message.from.first_name
     await ctx.replyWithMarkdown(`*Olá, ${nome}!*\nEu sou o Bot Sustain`)
+    await ctx.replyWithMarkdown(`Comando disponíveis para uso:
+/dauto - *Lista de chamados de auto para vencer na data atual.*
+/dmassificado - *Lista de chamados de massificados para vencer na data atual.*
+/dcarga - *Lista de chamados de cargas para vencer na data atual.*
+/dwebmethods - *Lista de chamados de webMethods para vencer na data atual.*
+/frota - *Lista de chamados de frota.*
+/novoendosso - *Lista de chamados de novo endosso.*
+/renovacao - *Lista de chamados de renovação.*
+ /bb - *Lista de chamados do BB.*
+ /novoportal - *Lista de chamados do novo mcc.*
+ /aauto - *Número de chamados de auto a vencer na data d+1.*
+ /amasssificado - *Número de chamados de massificados a vencer na data d+1.*
+ /chamadosantigo - *Lista de chamados com mais de 20 dias de abertura.*`)
 })
 
 //Retorna a lista de chamados de Auto através do comando.
@@ -189,11 +203,11 @@ bot.hears(/dauto/i, async ctx => {
             if (chamado.DataNextBreachOLA != null) {
                 if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                     var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-                    ctx.reply(`Incidente: ${chamado.IdIncidente} 
-Titulo: ${chamado.Titulo} 
-Prioridade: ${chamado.Prioridade} 
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+                    ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
                 }
             }
         })
@@ -220,11 +234,11 @@ bot.hears(/dmassificado/i, async ctx => {
             if (chamado.DataNextBreachOLA != null) {
                 if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                     var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-                    ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+                    ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
                 }
             }
         })
@@ -251,11 +265,11 @@ bot.hears(/dcargas/i, async ctx => {
             if (chamado.DataNextBreachOLA != null) {
                 if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                     var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-                    ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+                    ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
                 }
             }
         })
@@ -282,11 +296,11 @@ bot.hears(/dwebmethods/i, async ctx => {
             if (chamado.DataNextBreachOLA != null) {
                 if (moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY") == now) {
                     var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-                    ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+                    ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
                 }
             }
         })
@@ -320,11 +334,11 @@ bot.hears(/frota/i, async ctx => {
     listaChamadosAuto.map(chamado => {
         if (chamado.Titulo.includes("FR") || chamado.Titulo.includes("EFR")) {
             var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-            ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
         }
     })
 })
@@ -334,11 +348,11 @@ bot.hears(/novoendosso/i, async ctx => {
     listaChamadosAuto.map(chamado => {
         if (chamado.Titulo.includes("NEA")) {
             var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-            ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
         }
     })
 })
@@ -348,11 +362,11 @@ bot.hears(/renovacao/i, async ctx => {
     listaChamadosAuto.map(chamado => {
         if (chamado.Titulo.includes("RM")) {
             var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-            ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
         }
     })
 })
@@ -362,11 +376,11 @@ bot.hears(/bb/i, async ctx => {
     listaChamadosAuto.map(chamado => {
         if (chamado.Titulo.includes("BB")) {
             var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-            ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
         }
     })
 })
@@ -375,11 +389,11 @@ bot.hears(/novoportal/i, async ctx => {
     listaChamadosAuto.map(chamado => {
         if (chamado.Titulo.includes("NMCC")) {
             var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
-            ctx.reply(`Incidente: ${chamado.IdIncidente}
-Titulo: ${chamado.Titulo}
-Prioridade: ${chamado.Prioridade}
-Vencimento: ${vencimento}
-Status: ${chamado.Status}`)
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}*
+Titulo: *${chamado.Titulo}*
+Prioridade: *${chamado.Prioridade}*
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*`)
         }
     })
 })
@@ -396,10 +410,12 @@ bot.hears(/aauto/i, async ctx => {
         }
     })
     if (contador != 0) {
-        await ctx.reply(`Temos ${contador} para vencer na data de ${now}`)
+        await ctx.replyWithMarkdown(`Temos *${contador}* chamado(s) de auto para vencer na data de *${now}*`)
+    }
+    else {
+        await ctx.replyWithMarkdown(`Não temos chamados para vencer na data de *${now}*`)
     }
 })
-
 
 bot.hears(/amassificado/i, async ctx => {
     await getChamadosMassificados()
@@ -413,10 +429,33 @@ bot.hears(/amassificado/i, async ctx => {
         }
     })
     if (contador != 0) {
-        await ctx.reply(`Temos ${contador} para vencer na data de ${now}`)
+        await ctx.replyWithMarkdown(`Temos *${contador}* chamado(s) de massificados para vencer na data de *${now}*`)
+    }
+    else {
+        await ctx.replyWithMarkdown(`Não temos chamados para vencer na data de *${now}*`)
     }
 })
 
+bot.hears(/chamadosantigo/i, async ctx => {
+    const dia = moment().subtract(20, 'days').calendar()
+    listaChamadosGeral.map(chamado => {
+        if (dia >= moment(chamado.DataAbertura).format("MM/DD/YYYY")) {
+            var vencimento = moment(chamado.DataNextBreachOLA).format("DD/MM/YYYY HH:mm:ss")
+            var dataabertura = moment(chamado.DataAbertura).format("DD/MM/YYYY HH:mm:ss")
+            var dataAbertura = new Date(chamado.DataAbertura)
+            var idade = (new Date() - dataAbertura) / 24 / 60 / 60 / 1000
+            idade = parseFloat(idade).toFixed(0);
+            ctx.replyWithMarkdown(`Incidente: *${chamado.IdIncidente}* 
+Titulo: *${chamado.Titulo}* 
+Prioridade: *${chamado.Prioridade}* 
+Vencimento: *${vencimento}*
+Status: *${chamado.Status}*
+DataAbertura: *${dataabertura}*
+NomeGrupo: *${chamado.NomeGrupo}*
+Idade: *${idade} dias*`)
+        }
+    })
+})
 
 //Chamada do main.
 main();
